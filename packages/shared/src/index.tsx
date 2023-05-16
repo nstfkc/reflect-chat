a4a1c531dae46ca692589a18018759c8342c59b1
Enes Tufekci <enesxtufekci@gmail.com>
2023-05-16 20:37:41 +0200

    shared components

--- packages/shared/src/index.tsx
+++ packages/shared/src/index.tsx
@@ -3,23 +3,25 @@ export * from "./types/global";
 export {
   ConfigContext,
   ConfigProvider,
-} from "./context/ConfigContext/ConfigContext";
+} from "./components/context/ConfigContext/ConfigContext";
 
 export {
   MessageContext,
   MessageProvider,
-} from "./context/MessageContext/MessageContext";
+} from "./components/context/MessageContext/MessageContext";
 
 export {
   SocketContext,
   SocketProvider,
-} from "./context/SocketContext/SocketContext";
+} from "./components/context/SocketContext/SocketContext";
+export { useSocket } from "./components/context/SocketContext/useSocket";
 
-export { useSocket } from "./context/SocketContext/useSocket";
-
-export { UserContext, UserProvider } from "./context/UserContext/UserContext";
+export {
+  UserContext,
+  UserProvider,
+} from "./components/context/UserContext/UserContext";
 
 export {
   UsersContext,
   UsersProvider,
-} from "./context/UsersContext/UsersContext";
+} from "./components/context/UsersContext/UsersContext";
