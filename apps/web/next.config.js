a2a4e1dce2df67226b10b869df2db7707b0c32fa
Enes Tufekci <enesxtufekci@gmail.com>
2023-06-17 11:57:36 +0200

    web deploy

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -11,28 +11,27 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  transpilePackages: ["auth"],
 
-  rewrites: async () => {
-    return [
-      {
-        source: "/socket.io",
-        destination: `${socketHost}/socket.io/`,
-      },
-      {
-        source: "/socket.io/:path*",
-        destination: `${socketHost}/socket.io/:path*`,
-      },
-      {
-        source: "/media/:path*",
-        destination: `https://flak.flak-media.workers.dev/:path*`,
-      },
-      {
-        source: "/api/:path*",
-        destination: `http://localhost:8080/:path*`,
-      },
-    ];
-  },
+  // rewrites: async () => {
+  //   return [
+  //     {
+  //       source: "/socket.io",
+  //       destination: `${socketHost}/socket.io/`,
+  //     },
+  //     {
+  //       source: "/socket.io/:path*",
+  //       destination: `${socketHost}/socket.io/:path*`,
+  //     },
+  //     {
+  //       source: "/media/:path*",
+  //       destination: `https://flak.flak-media.workers.dev/:path*`,
+  //     },
+  //     {
+  //       source: "/api/:path*",
+  //       destination: `http://localhost:8080/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
