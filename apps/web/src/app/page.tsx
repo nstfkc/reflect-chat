d68f308c7d19ded7207909c3dad1cf3fc69ff69c
Enes Tufekci <enesxtufekci@gmail.com>
2023-06-17 11:23:47 +0200

    deploy website

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div className="h-full flex flex-col justify-center items-center">
-      HOME
-      {/* <AuthGuard>Hi</AuthGuard> */}
+      HOME reflect.rocks
     </div>
   );
 }
