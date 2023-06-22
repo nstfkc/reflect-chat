e6bdd00e203de57794bf12540fda76ebdc1a303f
Enes Tufekci <enesxtufekci@gmail.com>
2023-06-22 18:19:14 +0200

    cleanup

--- apps/web/src/app_backup/auth/sign-up/page.tsx
+++
@@ -1,7 +0,0 @@
-import { SignUp } from "@clerk/clerk-react";
-
-const SignUpPage = () => {
-  return <SignUp></SignUp>;
-};
-
-export default SignUpPage;
--- apps/web/src/app_backup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthGuard } from "./components/Auth";
-
-export default function Home() {
-  return (
-    <div className="h-full flex flex-col justify-center items-center">
-      <AuthGuard>Hi</AuthGuard>
-    </div>
-  );
-}
