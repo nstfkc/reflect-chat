d9852399eb2c88940b29209f27d7a09b316df7a8
Enes Tufekci <enesxtufekci@gmail.com>
2023-12-23 11:47:06 +0100

    fix server host

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -109,7 +109,7 @@ server.ready().then(() => {
   sockets(server.io);
 });
 
-server.listen({ port: 8080 }, (err, address) => {
+server.listen({ port: 8080, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
